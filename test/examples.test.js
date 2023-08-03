08ce6332a84e41e880c5bacf8a58376de177a3f5
Mike Rombout <mike.rombout1@gmail.com>
2023-08-04 01:19:41 +0200

    Increase visual test tolerance to 9

--- test/examples.test.js
+++ test/examples.test.js
@@ -39,7 +39,7 @@ fs.readdirSync(examplesDir).forEach((exampleDir) => {
 
       const { equal, diffImage, differentPixels } = await looksSame(expectedOutputImgFile, actualOutputImgFile, {
         createDiffImage: true,
-        tolerance: 8
+        tolerance: 9
       })
 
       if (!equal) {
