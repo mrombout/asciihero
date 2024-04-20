88409e07458a27be44bcfd213fc1397fb8a9c783
Mike Rombout <mike.rombout1@gmail.com>
2024-04-20 09:12:02 +0200

    Update thumbnail to show more interesting pages

--- utils/example_thumbnails.js
+++ utils/example_thumbnails.js
@@ -6,7 +6,21 @@ const pdf2img = require('pdf-img-convert')
 const looksSame = require('looks-same')
 const path = require('path')
 const chai = require('chai')
+const { example } = require('yargs')
 const expect = chai.expect
+
+const thumbnailPages = {
+  "attributes": [2],
+  "dice_footer": [3],
+  "dice_table": [2],
+  "images": [2],
+  "input": [3],
+  "manual_segment_number": [3],
+  "shuffle": [3],
+  "simple": [3],
+  "stable_segments": [3],
+  "stylesheet": [7],
+}
 
 const examplesDir = './examples'
 fs.readdirSync(examplesDir).forEach(async (exampleDir) => {
@@ -18,7 +32,7 @@ fs.readdirSync(examplesDir).forEach(async (exampleDir) => {
   const pdf2imgOpts = {
     width: 620,
     height: 877,
-    page_numbers: [1]
+    page_numbers: thumbnailPages.hasOwnProperty(exampleDir) ? thumbnailPages[exampleDir] : [2]
   }
   const expectedOutputImgs = await pdf2img.convert(pdfFile, pdf2imgOpts)
   for (expectedOutputImg of expectedOutputImgs) {
